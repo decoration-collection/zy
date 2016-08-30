c35a2669e8d4aad71af193ca49bc76711eb7fa24
karl <ideexto@gmail.com>
2016-08-30 19:15:50 +0800

    push some files and some configurations.

--- zy-web/src/main/java/com/zy/web/UserController.java
+++ zy-web/src/main/java/com/zy/web/UserController.java
@@ -4,8 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -18,6 +20,16 @@ public class UserController {
 		map.put("code", "hello world");
 		
 		return map;
+	}
+
+	@RequestMapping(value="/admin")
+	public ModelAndView index(ModelMap modelMap) {
+
+		modelMap.put("xxx", "guage");
+//		return "/admin/admin.html";
+
+		ModelAndView view = new ModelAndView("admin/admin",modelMap);
+		return view;
 	}
 	
 }
