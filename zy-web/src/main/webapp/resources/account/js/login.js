3d1b17732c49d3bf63d237e8d948e7e8273b3b4c
karl <ideexto@gmail.com>
2016-09-28 10:11:24 +0800

    init account login page

---
+++ zy-web/src/main/webapp/resources/account/js/login.js
@@ -0,0 +1,13 @@
+/**
+ * Login JS
+ * @module Login
+ * @author Karl Luo
+ * @Date 2016-09-27
+ */
+$(function () {
+    $('input').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue',
+        increaseArea: '20%' // optional
+    });
+});
