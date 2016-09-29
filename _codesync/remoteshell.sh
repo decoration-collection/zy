4a2ac38ed6141c222f6b289e79b07a00dc0975bd
zhangqiaojuan <1165229747@qq.com>
2016-09-29 11:25:07 +0800

    delete _codesync

--- _codesync/remoteshell.sh
+++
@@ -1,3 +0,0 @@
-. /etc/profile; cd /home/zhenyuanzs/data/zy/zy-web/src/main/webapp/resources && rm -rf static && /usr/bin/fis3 release -d ./static
-sleep 3
-cp -r /home/zhenyuanzs/data/zy/zy-web/src/main/webapp/* /home/zhenyuanzs/tomcat/apache-tomcat-7.0.70_dev/webapps/zy
