755662e6bf93aefda1b932547842f6a74c10894c
zhangqiaojuan <1165229747@qq.com>
2016-09-30 11:46:04 +0800

    update craft page

---
+++ zy-web/src/main/webapp/resources/admin/js/crafts/all_crafts.js
@@ -0,0 +1,10 @@
+$(function(){
+	$('#all_crafts').DataTable({
+		"paging": true,
+	    "lengthChange": false,
+	    "searching": false,
+	    "ordering": false,
+	    "info": true,
+	    "autoWidth": false
+	});
+});
